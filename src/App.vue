c0ae75d92822707d2cc75b51a9cc2df5e1a99c72
CodeSandbox <support@codesandbox.io>
2022-11-23 15:11:11 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <List :data="data" />
+  </div>
+</template>
+<script>
+import List from "./components/List.vue";
+
+export default {
+  data() {
+    return {
+      data: [
+        {
+          name: "Anna",
+          year: 10,
+          class: "A",
+        },
+        {
+          name: "Mark",
+          year: 10,
+          class: "M",
+        },
+      ],
+    };
+  },
+  components: {
+    List,
+  },
+};
+</script>
---
+++ src/components/List.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <ul v-if="listType === 1">
+      <li v-for="item in data" :key="item.name" class="list1">
+        {{ item.name }} - {{ item.year }} - {{ item.class }}
+      </li>
+    </ul>
+    <ul v-else-if="listType === 2">
+      <li v-for="item in data" :key="item.title">
+        {{ item.title }} - {{ item.category }} - {{ item.rating }}
+      </li>
+    </ul>
+    <ul v-else-if="listType === 3">
+      <li v-for="item in data" :key="item.rank">
+        {{ item.rank }} - {{ item.winner }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    listType() {
+      if (this.data.length === 0) {
+        return null;
+      }
+      if (typeof this.data[0].name !== "undefined") {
+        return 1;
+      } else if (typeof this.data[0].title !== "undefined") {
+        return 2;
+      } else if (typeof this.data[0].winner !== "undefined") {
+        return 3;
+      } else {
+        return null;
+      }
+    },
+  },
+};
+</script>
